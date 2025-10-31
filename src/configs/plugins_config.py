238e63c18659c7ccf221fa181af2dab22f447b16
tau-lavender <=>
2025-11-01 00:50:38 +0300

    added mv

--- src/configs/plugins_config.py
+++ src/configs/plugins_config.py
@@ -3,4 +3,5 @@ PLUGINS_IMPORT_CONFIG = ["src.base_commands.ls",
                          "src.base_commands.cat",
                          "src.base_commands.rm",
                          "src.base_commands.cp",
+                         "src.base_commands.mv",
                          ]
