21d4a49deccc36c63cca165fca07c03ecc9e4544
tau-lavender <=>
2025-11-01 03:04:35 +0300

    log file name fixed

--- src/configs/logger_config.py
+++ src/configs/logger_config.py
@@ -12,7 +12,7 @@ LOGGING_CONFIG = {
             "class": "logging.handlers.RotatingFileHandler",
             "formatter": "standard",
             "mode": "a",
-            "filename": "app.log",
+            "filename": "shell.log",
             "maxBytes": 5 * 1024 * 1024,  # 5 MB before rotating
             "backupCount": 5,
             "level": "DEBUG",
